2dffcbbf9174aba6e8fe3334ec6ca66cf57d81ff
Mateus Zitelli <zitellimateus@gmail.com>
2014-02-09 17:58:08 -0200

    Definindo nas configurações do compass a geração de CSS minificado

--- config.rb
+++ config.rb
@@ -3,4 +3,4 @@ css_dir = "themes/bootstrap3/assets/css/"
 sass_dir = "themes/bootstrap3/assets/sass/"
 images_dir = "themes/bootstrap3/assets/img/"
 javascript_dit = "themes/bootstrap3/assets/js"
-output_style = :compact
+output_style = :compressed
